3e70f27000965f7cfc509fc764aac069cd273115
Oriol VidalCortes <ovidalcortes@gmail.com>
2019-04-21 13:46:54 +0200

    first launch

---
+++ main.js
@@ -0,0 +1,47 @@
+
+const {app, BrowserWindow} = require('electron')
+
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
+
+
+const mainMenuTemplate = [
+  {
+    label: 'Main'
+  }
+]
+
+// const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+// Menu.setApplicationMenu(mainMenu);
+
